598a113b8c06552635322d11bb9c2c470dc6d409
warzes <warzes@mail.ru>
2018-12-07 16:54:45 +1000

    - Str

---
+++ Source/Core/Str.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+//=============================================================================
+SE_NAMESPACE_BEGIN
+
+template <int size> 
+class StaticString
+{
+public:
+	StaticString() { m_data[0] = '\0'; }
+
+	bool Empty() const { return m_data[0] == '\0'; }
+
+	operator const char*() const { return m_data; }
+
+private:
+	char m_data[size];
+};
+
+SE_NAMESPACE_END
+//=============================================================================
